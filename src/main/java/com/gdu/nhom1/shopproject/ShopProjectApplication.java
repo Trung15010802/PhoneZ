f66995c7efa99cb5655c57124a19efaea02f5386
Trung Bùi <handsomezezo@gmail.com>
2023-03-14 09:00:35 +0700

    Update UI

--- src/main/java/com/gdu/nhom1/shopproject/ShopProjectApplication.java
+++ src/main/java/com/gdu/nhom1/shopproject/ShopProjectApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-
-
 public class ShopProjectApplication {
 
 	public static void main(String[] args) {
